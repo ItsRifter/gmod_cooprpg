cb3ce671b35d85ab7f6bc0c86fca08d788bf597f
ItsRifter <d_thomas_smith30@hotmail.com>
2024-01-08 15:36:43 +0000

    lamba > lambda + inv weapon suppression

--- entities/entities/hl2c_lambda/init.lua
+++ entities/entities/hl2c_lambda/init.lua
@@ -4,7 +4,7 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Initialize()
-	self:SetModel("models/hl2c/lamba_logo.mdl")
+	self:SetModel("models/hl2c/lambda_logo.mdl")
 	self:SetRenderMode( RENDERMODE_TRANSALPHA )
 	self:SetMaterial("hl2c/x64")
 	self:DrawShadow( false)
--- gamemode/server/player/sv_player_inventory.lua
+++ gamemode/server/player/sv_player_inventory.lua
@@ -16,16 +16,21 @@ function hl2c_player:GiveWeapons()
     end
 end
 
+--Prevents a endless supply of new weapons to players
+local suppress_NewWeapons = false
+
 function AddWeaponRespawns(strWepClass, bGiveOthers)
-    if table.HasValue( HL2C_Server.AvailableWeapons, strWepClass ) then return end
-	table.insert(HL2C_Server.AvailableWeapons, strWepClass)
-
-    HL2C_Server:DebugMsg(strWepClass, 0)
-
+    if not table.HasValue( HL2C_Server.AvailableWeapons, strWepClass ) then 
+	    table.insert(HL2C_Server.AvailableWeapons, strWepClass)
+        bGiveOthers = true
+    end
+    //HL2C_Server:DebugMsg(strWepClass, 0)
     if bGiveOthers then
+        suppress_NewWeapons = true
         for _, p in ipairs(player.GetAll()) do
             if !p:HasWeapon( strWepClass ) then p:Give(strWepClass) end
         end
+        suppress_NewWeapons = false
     end
 end
 
@@ -37,6 +42,9 @@ hook.Add("PlayerCanPickupWeapon", "hl2c_inv_weaponcheck", function(ply, wep)
         else return false end
     end
 
-    AddWeaponRespawns(wep:GetClass())
+    if not suppress_NewWeapons then
+        AddWeaponRespawns(wep:GetClass())
+    end
+
     return true
 end)
