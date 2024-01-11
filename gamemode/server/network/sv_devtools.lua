5927df3048107787609dc71b45546caceb4e7d95
NekoBaron <nekobaronuk@gmail.com>
2024-01-11 23:21:18 +0000

    Update sv_devtools.lua
    added ent info scan command and also new deep scan for finding invisible brushes like triggers.

--- gamemode/server/network/sv_devtools.lua
+++ gamemode/server/network/sv_devtools.lua
@@ -14,3 +14,21 @@ net.Receive( "HL2C_DEV_DestroyCPs", function( length, ply )
 end )
 
 
+--reports entity being looked at
+concommand.Add("hl2c_entinfo", function(ply)
+	if !ply:IsSuperAdmin() then return end
+	local Ent = ply:GetEyeTrace().Entity
+	if IsValid(Ent) then
+		print(Ent)
+		print("Name  "..Ent:GetName())
+		print("Model "..Ent:GetModel())
+		if IsValid(Ent:GetParent()) then print("Parent "..Ent:GetParent():GetName()) end
+	end
+end)
+
+--performs a deep ent scan, for finding anything like triggers.
+concommand.Add("hl2c_scan", function(ply)
+	if !ply:IsSuperAdmin() then return end
+	local scan = ents.FindAlongRay( ply:EyePos(), ply:EyePos() + ply:EyeAngles():Forward() * 128)
+	PrintTable(scan)
+end)
