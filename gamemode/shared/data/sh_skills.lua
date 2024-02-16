eb074fac875c44b964feafeff23d7b38c148939b
NekoBaron <nekobaronuk@gmail.com>
2024-02-16 00:39:26 +0000

    Create sh_skills.lua
    created WIP skills table for proposed skill ideas

---
+++ gamemode/shared/data/sh_skills.lua
@@ -0,0 +1,45 @@
+--Table data will be empty for a while, its just a list of possible skills we could add
+local HL2C_Skills = {
+	["hpboost_1"]={			--Increase HP slightly
+	},
+	["hpboost_2"]={			--Increase HP slightly
+	},
+	["hpboost_3"]={			--Increase HP slightly
+	},
+	["armor_1"]={			--Increase Armour slightly
+	},
+	["armor_2"]={			--Increase Armour slightly
+	},
+	["armor_3"]={			--Increase Armour slightly
+	},
+	["flashdrain_1"]={		--Decrease flashlight drain
+	},
+	["flashdrain_2"]={		--Decrease flashlight drain
+	},
+	["Ammo_1"]={			--Prevent wasted ammo
+	},
+	["Ammo_2"]={			--Extra carry clip
+	},
+	["Ammo_3"]={			--Start map prepared
+	},
+	["sprintdrain"]={		--Decrease Sprintdrain
+	},
+	["sprintrecover"]={		--Recover sprint earlier
+	},
+	["regenstamina"]={		--Increase stamina regen
+	},
+	["regenbattery"]={		--Increase battery regen
+	},
+	["regenhealth_1"]={		--Give health regen
+	},
+	["regenhealth_2"]={		--Give more health regen
+	},
+	["loadout_1"]={			--Unlock setting different melee weapon?			give a starting one?
+	},
+	["loadout_2"]={			--Unlock setting different bullet weapons?			give a starting one?
+	},
+	["vehicle_1"]={			--Unlock setting custom lamp colours on vehicle?	give a few starting ones?
+	},
+	["vehicle_2"]={			--Unlock setting custom colour/skin on vehicle?		give a starting one?
+	},
+}
